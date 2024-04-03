e3bdd1cd5aa1b5a238ed56b08d3dab34138ab5f7
dabardibid <32200281+DavidJoacaRo@users.noreply.github.com>
2024-04-03 20:58:28 +0200

    Attempt at exposing SteamUtils to move Steam Notification Position
    With a little bit of help from @r0neko, but we failed since IT CANNOT GET <steam/steam_api.h> or so

---
+++ Source/BudgetBackrooms/Public/UBBSteamUtils.h
@@ -0,0 +1,32 @@
+
+
+#pragma once
+
+#include "OnlineSubsystemSteam.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "CoreMinimal.h"
+//#include "UBBSteamUtils.generated.h"
+/**
+
+
+
+UENUM(BlueprintType)
+enum class ESteamNotificationPosition : uint8
+{
+    TopLeft UMETA(DisplayName = "Top Left"),
+    TopRight UMETA(DisplayName = "Top Right"),
+    BottomLeft UMETA(DisplayName = "Bottom Left"),
+    BottomRight UMETA(DisplayName = "Bottom Right")
+};
+
+UCLASS()
+class BUDGETBACKROOMS_API UBBSteamUtils : public UBlueprintFunctionLibrary
+{
+    GENERATED_UCLASS_BODY() 
+
+public:
+    UBBSteamUtils(const FPostConstructInitializeProperties& PCIP);
+    UFUNCTION(BlueprintCallable, Category = "Steam")
+    static void SetSteamOverlayNotificationPosition(ESteamNotificationPosition Position);
+};
+ */
