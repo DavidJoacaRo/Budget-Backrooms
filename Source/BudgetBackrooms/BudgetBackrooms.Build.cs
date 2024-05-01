fd1e8b75c5d7f2e31a141dd773ca6c45294ea261
dabardibid <32200281+DavidJoacaRo@users.noreply.github.com>
2024-05-01 09:20:06 +0200

    i give up on all this tomfoolery

--- Source/BudgetBackrooms/BudgetBackrooms.Build.cs
+++ Source/BudgetBackrooms/BudgetBackrooms.Build.cs
@@ -11,9 +11,6 @@ public class BudgetBackrooms : ModuleRules
 		// Uncomment if you are using Slate UI
 		PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore", "RHI" });
 
-        // Uncomment if you are using online features
-        //PrivateDependencyModuleNames.AddRange(new string[] { "OnlineSubsystem", "OnlineSubsystemSteam" });
-
         // To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
     }
 }
---
+++ Source/BudgetBackrooms/Private/UBBSteamUtils.cpp
@@ -0,0 +1,38 @@
+
+
+#include "UBBSteamUtils.h"
+#include "CoreMinimal.h"
+
+/* 
+UBBSteamUtils::UBBSteamUtils(const class FPostConstructInitializeProperties& PCIP) 
+    : Super(PCIP)
+{
+    
+} 
+
+void UBBSteamUtils::SetSteamOverlayNotificationPosition(ESteamNotificationPosition Position)
+{
+    ENotificationPosition steamPosition = k_EPositionBottomRight;
+
+    switch (Position)
+    {
+    case ESteamNotificationPosition::TopLeft:
+        steamPosition = k_EPositionTopLeft;
+        break;
+    case ESteamNotificationPosition::TopRight:
+        steamPosition = k_EPositionTopRight;
+        break;
+    case ESteamNotificationPosition::BottomLeft:
+        steamPosition = k_EPositionBottomLeft;
+        break;
+    case ESteamNotificationPosition::BottomRight:
+        steamPosition = k_EPositionBottomRight;
+        break;
+    default:
+        break;
+    }
+
+    SteamUtils()->SetOverlayNotificationPosition(steamPosition);
+}
+
+*/
