beeb39cb783cb6b7b75ed07800f0ba8d6f43ef9b
dabardibid <32200281+DavidJoacaRo@users.noreply.github.com>
2024-08-01 18:59:50 +0200

    Supposedly half-baked controller support

--- Source/BudgetBackrooms/Private/UBBControllerUtils.cpp
+++ Source/BudgetBackrooms/Private/UBBControllerUtils.cpp
@@ -3,11 +3,30 @@
 #include "GameFramework/PlayerController.h"
 #include "GameFramework/InputSettings.h"
 #include "GenericPlatform/GenericPlatformDriver.h"
+#include "TimerManager.h"
 
 void UBBControllerUtils::IsGamepadConnected(bool& IsGamepadAttached)
 {
     auto genericApplication = FSlateApplication::Get().GetPlatformApplication();
-
     IsGamepadAttached = genericApplication.Get() != nullptr && genericApplication->IsGamepadAttached();
+}
 
+
+void UBBControllerUtils::SetControllerVibration(int32 ControllerId, float Intensity, float Duration)
+{
+    if (GEngine)
+    {
+        APlayerController* PC = GEngine->GetFirstLocalPlayerController(GWorld);
+        if (PC && PC->IsLocalController())
+        {
+            // Set the vibration intensity
+            PC->PlayDynamicForceFeedback(Intensity, Duration, true, true, true, true);
+
+            // Optionally, you could use a timer to stop the vibration after the duration
+            FTimerHandle TimerHandle;
+            PC->GetWorldTimerManager().SetTimer(TimerHandle, [PC]() {
+                PC->PlayDynamicForceFeedback(0.f, 0.f, false, false, false, false);
+                }, Duration, false);
+        }
+    }
 }
--- Source/BudgetBackrooms/Public/UBBControllerUtils.h
+++ Source/BudgetBackrooms/Public/UBBControllerUtils.h
@@ -11,7 +11,9 @@ class BUDGETBACKROOMS_API UBBControllerUtils : public UBlueprintFunctionLibrary
 
 public:
     // Function to check if a controller is connected
-    UFUNCTION(BlueprintPure, Category = "BB Controller Utils", meta = (Keywords = "Check Gamepad Connection", ToolTip = "Check if any Gamepad is connected & detected by the OS driver. :nerd:"))
+    UFUNCTION(BlueprintPure, Category = "Budget Controller Utils", meta = (Keywords = "Check Gamepad Connection", ToolTip = "Check if any Gamepad is connected & detected by the OS driver. :nerd:"))
         static void IsGamepadConnected(bool& IsGamepadAttached);
-    
+
+    UFUNCTION(BlueprintCallable, Category = "Budget Controller Utils", meta = (Keywords = "Rumble Gamepad", ToolTip = "Rumbles the connected gamepad for an amount of ms"))
+        static void SetControllerVibration(int32 ControllerId, float Intensity, float Duration);
 };
