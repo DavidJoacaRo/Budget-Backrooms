61d72b9835d6cebaf655e3cbb39d5c74c483933f
dabardibid <32200281+DavidJoacaRo@users.noreply.github.com>
2024-08-25 14:34:34 +0200

    Bulk changes no changelog, fixed AI being slow af

---
+++ Source/BudgetBackrooms/Public/UBBWindowsUtils.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "UBBWindowsUtils.generated.h"
+
+
+
+
+UENUM(BlueprintType)
+enum class EWindowsMessageBoxButtons : uint8
+{
+    OK UMETA(DisplayName = "OK"),
+    OKCancel UMETA(DisplayName = "OK Cancel"),
+    YesNo UMETA(DisplayName = "Yes No"),
+    YesNoCancel UMETA(DisplayName = "Yes No Cancel"),
+    RetryCancel UMETA(DisplayName = "Retry Cancel"),
+    AbortRetryIgnore UMETA(DisplayName = "Abort Retry Ignore")
+};
+
+
+UCLASS()
+class BUDGETBACKROOMS_API UBBWindowsUtils : public UBlueprintFunctionLibrary
+{
+    GENERATED_BODY()
+
+public:
+    UFUNCTION(BlueprintCallable, Category = "Budget Windows Utils")
+    static int32 ShowWindowsMessageBox(FString Message, FString Title, EWindowsMessageBoxButtons ButtonType);
+};
