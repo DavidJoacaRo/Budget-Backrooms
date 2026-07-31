8b277a598b6cfcec8d015c67248a62964e259c5f
dabardibid <32200281+dabardibid@users.noreply.github.com>
2026-07-31 02:12:22 +0200

    [+] Add level-based background for the Main Menu

--- Source/BudgetBackrooms/BBHashThing.h
+++ Source/BudgetBackrooms/BBHashThing.h
@@ -1,3 +1,3 @@
 #pragma once
-#define PROJECT_GIT_HASH "3c427d01"
-#define PROJECT_GIT_HASH_LONG "3c427d01a427ccf26cd48aaa673cacf2ceb46451"
+#define PROJECT_GIT_HASH "1fd17ca8"
+#define PROJECT_GIT_HASH_LONG "1fd17ca8fba7ad3511c0c1b2a0deb91e4284b116"
